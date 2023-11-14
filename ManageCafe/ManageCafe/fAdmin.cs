360d1750195497706ac339427f32ae7b5cc4ca34
KuroN3ko276 <nghaidang276@gmail.com>
2023-11-14 21:32:04 +0700

    First Commit
    Commit dau

---
+++ ManageCafe/ManageCafe/fAdmin.cs
@@ -0,0 +1,45 @@
+﻿using ManageCafe.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManageCafe
+{
+    public partial class fAdmin : Form
+    {
+        public fAdmin()
+        {
+            InitializeComponent();
+            LoadAccountList();
+        }
+
+        void LoadAccountList()
+        {
+            string query = "EXEC dbo.USP_GetAccountByUserName @userName";
+
+            DataProvider provider = new DataProvider();
+
+            dtgvAccount.DataSource = provider.ExecuteQuery(query, new object[] {"staff"});
+
+        }
+
+        //void loadfoodcategory()
+        //{
+        //    string query = "select * from dbo.foodcategory";
+
+        //    dataprovider provider = new dataprovider();
+
+        //    dtgvcategory.datasource = provider.executequery(query);
+
+        //}
+
+
+
+    }
+}
